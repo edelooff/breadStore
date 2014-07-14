6b6425daa3eca1d37f484b48a014e1444c853330
Elmer de Looff <elmer.delooff@gmail.com>
2014-07-14 23:25:17 +0200

    Removed extraneous imports and restricted main view to Root resource.

--- breadstore/views/__init__.py
+++ breadstore/views/__init__.py
@@ -1,18 +1,9 @@
 """breadStore API views."""
 
-# Standard modules
-import operator
-
 # Third-party modules
-from pyramid import httpexceptions as exc
-from pyramid import security
 from pyramid.view import view_config
-from pyramid.view import view_defaults
-
-# Application modules
-from . import models
 
 
-@view_config(renderer='json')
+@view_config(context='..resources.Root', renderer='json')
 def root(request):
   return {'application': 'breadStore'}
