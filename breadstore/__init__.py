aebe07cb8b091f598cadf8693d425ff0994d06d5
Elmer de Looff <elmer.delooff@gmail.com>
2014-07-14 21:20:24 +0200

    Added authentication and authorization factories.

--- breadstore/__init__.py
+++ breadstore/__init__.py
@@ -1,6 +1,8 @@
 """Web API for breadStore."""
 
 # Third-party modules
+from pyramid import authentication
+from pyramid import authorization
 from pyramid import renderers
 from pyramid.config import Configurator
 import simplejson
@@ -25,6 +27,18 @@ def main(global_config, **settings):
   config = Configurator(settings=settings)
   config.add_renderer('json', renderers.JSON(serializer=simplejson.dumps))
   config.add_request_method(request_scoped_session, 'db', reify=True)
+  config.set_authentication_policy(
+      authentication.AuthTktAuthenticationPolicy(
+          config.registry.settings['authentication_secret'],
+          callback=security.group_finder,
+          cookie_name='bs_auth',
+          hashalg='sha512',
+          http_only=True,
+          reissue_time=3600,
+          timeout=24 * 3600,
+          wild_domain=True))
+  config.set_authorization_policy(authorization.ACLAuthorizationPolicy())
+
   config.registry.dbmaker = sqlalchemy.orm.sessionmaker(
       bind=sqlalchemy.engine_from_config(settings),
       extension=zope.sqlalchemy.ZopeTransactionExtension())
