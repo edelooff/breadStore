d77cf553631c0ad126fca4164490dc406adc93d5
Elmer de Looff <elmer.delooff@gmail.com>
2014-07-19 23:10:45 +0200

    Removed simple json converting function in customer API vieuws.

--- breadstore/views/customer.py
+++ breadstore/views/customer.py
@@ -20,7 +20,7 @@ class CustomerCollectionView(object):
   @view_config(request_method='GET', permission='view')
   def list(self):
     """Returns a list of customers in the system."""
-    return {'klanten': map(record_to_json, self.request.db.query(models.Klant))}
+    return {'klanten': self.request.db.query(models.Klant).all()}
 
 
 @view_defaults(context='..resources.Customer')
@@ -33,15 +33,4 @@ class CustomerView(object):
   @view_config(request_method='GET', permission='view')
   def get(self):
     """Returns the information of a customer."""
-    return {'klant': record_to_json(self.customer)}
-
-
-def record_to_json(record):
-  """Turns an SQLA record instance into a simple dictionary for JSON usage."""
-  def get(attr):
-    value = getattr(record, attr)
-    if isinstance(value, (datetime.date, datetime.datetime)):
-      return value.isoformat()
-    return value
-
-  return {attr: get(attr) for attr in vars(record) if not attr.startswith('_')}
+    return {'klant': self.customer}
